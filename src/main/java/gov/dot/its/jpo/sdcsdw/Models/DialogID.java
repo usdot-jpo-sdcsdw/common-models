8007411a5f44e2633f0313d0e5d0ce38638b4dc4
Andrew M Melnick <andrew.m.melnick@aero.org>
2019-02-26 12:37:49 -0500

    Add JsonProperties to DialogID so that the string values match the spec

--- src/main/java/gov/dot/its/jpo/sdcsdw/Models/DialogID.java
+++ src/main/java/gov/dot/its/jpo/sdcsdw/Models/DialogID.java
@@ -5,26 +5,38 @@ import javax.xml.bind.annotation.XmlEnumValue;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @XmlJavaTypeAdapter(DialogIDXmlAdapter.class)
 public enum DialogID {
+	@JsonProperty("vehSitData")
 	VEH_SIT_DATA("vehSitData"),
 	
+	@JsonProperty("dataSubscription")
 	DATA_SUBSCRIPTION("dataSubscription"),
 	
+	@JsonProperty("advSitDataDep")
 	ADV_SIT_DATA_DEP("advSitDataDep"),
 	
+	@JsonProperty("advSitDatDist")
 	ADV_SIT_DAT_DIST("advSitDatDist"),
 	
+	@JsonProperty("reserved1")
 	RESERVED_1("reserved1"),
 	
+	@JsonProperty("reserved2")
 	RESERVED_2("reserved2"),
 	
+	@JsonProperty("objReg")
 	OBJ_REG("objReg"),
 	
+	@JsonProperty("objDisc")
 	OBJ_DISC("objDisc"),
 	
+	@JsonProperty("intersectionSitDataDep")
 	INTER_SIT_DATA_DEP("intersectionSitDataDep"),
 	
+	@JsonProperty("intersectionSitDataQuery")
 	INTER_SIT_DATA_QUERY("intersectionSitDataQuery");
 	
 	private final String value;
